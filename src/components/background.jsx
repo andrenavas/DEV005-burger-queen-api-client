a6f9869a44630224b70d24451f7460c2a6d6d7c5
andrenavas <andreynanavas@gmail.com>
2023-06-13 17:12:20 -0500

    UPDATE: login with error message and first steps of waiter view

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -1,14 +1,13 @@
 import './background.css';
+
 const Background = () => {
   return (
     <>
       <div className ="background-app">
         <div className ='up-options'>
           <nav className = 'nav-waiter'>
-          <h1>Mesero</h1>
+            
           </nav>
-
-         
         </div>
       </div>
     </>
